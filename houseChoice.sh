ed171d93326c558eab9ee766860e5706eb8650e8
jplowri0 <jplowri0@our.ecu.edu.au>
2020-07-23 09:47:01 -0400

    house table prettued

--- houseChoice.sh
+++ houseChoice.sh
@@ -6,5 +6,18 @@ read -p "State: " state
 echo "Now enter a Congressional district number"
 read -p "District: " district
 
-echo $state
-echo $district
+
+#need to filter the weball20 file by parsing the state and district variables.
+#REF A & REF B REF C
+
+awk -v st="$state" -v di="$district" -F "|" '{OFS="\t"; if(($19==st) && ($20==di)) print $2, $5, $6, $8}' weball20.txt > filtered$state$district.txt
+
+#this awk command below is still formatted from the userAccounts.sh from school.
+awk 'BEGIN {
+    FS="\t";
+    printf ("| \033[34m%-30s\033[0m | \033[34m%-8s\033[0m | \033[34m%-20s\033[0m | \033[34m%-20s\033[0m |\n", "Candidate", "Party", "Received", "Spent");
+    print "|________________________________|__________|______________________|______________________|";
+}
+{
+    printf("| \033[33m%-30s\033[0m | \033[35m%-8s\033[0m | \033[35m%-20s\033[0m | \033[35m%-20s\033[0m |\n", $1, $2, $3, $4);
+}' filtered$state$district.txt
